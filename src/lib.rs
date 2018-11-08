625de6658f7417dc94dc0f9b461dcd95dd4245e9
Robert Węcławski <r.weclawski@gmail.com>
2018-11-08 22:51:40 +0100

    fixed dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+#[cfg(feature = "dx12")]
+extern crate gfx_backend_dx12;
+#[cfg(not(any(feature = "vulkan", feature = "dx12", feature = "metal", feature = "gl")))]
+extern crate gfx_backend_empty;
+#[cfg(feature = "gl")]
+extern crate gfx_backend_gl;
+#[cfg(feature = "metal")]
+extern crate gfx_backend_metal;
+#[cfg(feature = "vulkan")]
+extern crate gfx_backend_vulkan;
+
+extern crate winit;
+extern crate gfx_hal as hal;
+extern crate glsl_to_spirv;
+
+pub mod render;
+
+use render::{Renderer};
+
 /// A struct representing the top level of this library.
 /// It provides access to all the subsystems that can be used.
 pub struct Smml {
--- src/render/renderer.rs
+++ src/render/renderer.rs
@@ -1,3 +1,41 @@
+#[cfg(feature = "dx12")]
+use gfx_backend_dx12 as backend;
+#[cfg(feature = "gl")]
+use gfx_backend_gl as backend;
+#[cfg(feature = "metal")]
+use gfx_backend_metal as backend;
+#[cfg(feature = "vulkan")]
+use gfx_backend_vulkan as backend;
+
+#[cfg(feature = "gl")]
+use backend::glutin::GlContext;
+
+use winit::{EventsLoop, WindowBuilder};
+
+// settings
+const SCR_WIDTH: f64 = 800.0;
+const SCR_HEIGHT: f64 = 600.0;
+
+struct WindowState {
+    events_loop: EventsLoop,
+    wb: WindowBuilder,
+}
+
+impl WindowState {
+    fn new() -> WindowState {
+        let events_loop = winit::EventsLoop::new();
+
+        let wb = winit::WindowBuilder::new()
+            .with_dimensions(winit::dpi::LogicalSize::new(DIMS.width as _, DIMS.height as _))
+            .with_title("quad".to_string());
+
+        WindowState {
+            events_loop: events_loop,
+            wb: wb,
+        }
+    }
+}
+
 pub struct Renderer {
 
 }
