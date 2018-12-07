713241ccaabbfeeb285aa328cc4e4515f909b5b2
Robert Węcławski <r.weclawski@gmail.com>
2018-12-07 08:48:56 +0100

    object class

--- src/resource/object.rs
+++ src/resource/object.rs
@@ -1,4 +1,7 @@
+#[derive(Clone)]
 pub struct Object<T> {
+    name: String,
+
     position_x: f32,
     position_y: f32,
     position_z: f32,
@@ -14,13 +17,96 @@ pub struct Object<T> {
     inner: &T,
 }
 
-impl<T> Object<T> {
-    pub fn new(inner: &T) {
 
+impl<T> Object<T> {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn set_name<S: Into<String>>(&mut self, name: S) {
+        self.name = name.into();
+    }
+
+    pub fn set_position_x(&mut self, position_x: f32) {
+        self.position_x = position_x;
+    }
+
+    pub fn set_position_y(&mut self, position_y: f32) {
+        self.position_y = position_y;
+    }
+
+    pub fn set_position_z(&mut self, position_z: f32) {
+        self.position_z = position_z;
+    }
+
+    pub fn set_rotation_angle_x(&mut self, rotation_angle_x: f32) {
+        self.rotation_angle_x = rotation_angle_x;
+    }
+
+    pub fn set_rotation_angle_y(&mut self, rotation_angle_y: f32) {
+        self.rotation_angle_y = rotation_angle_y;
+    }
+
+    pub fn set_rotation_angle_z(&mut self, rotation_angle_z: f32) {
+        self.rotation_angle_z = rotation_angle_z;
+    }
+
+    pub fn set_scale_x(&mut self, scale_x: f32) {
+        self.scale_x = scale_x;
+    }
+
+    pub fn set_scale_y(&mut self, scale_y: f32) {
+        self.scale_y = scale_y;
+    }
+
+    pub fn set_scale_z(&mut self, scale_z: f32) {
+        self.scale_z = scale_z;
+    }
+
+    pub fn position_x(&self) -> f32 {
+        self.position_x
+    }
+
+    pub fn position_y(&self) -> f32 {
+        self.position_y
+    }
+
+    pub fn position_z(&self) -> f32 {
+        self.position_z
+    }
+
+    pub fn rotation_angle_x(&self) -> f32 {
+        self.rotation_angle_x
+    }
+
+    pub fn rotation_angle_y(&self) -> f32 {
+        self.rotation_angle_y
+    }
+
+    pub fn rotation_angle_z(&self) -> f32 {
+        self.rotation_angle_z
+    }
+
+    pub fn scale_x(&self) -> f32 {
+        self.scale_x
+    }
+
+    pub fn scale_y(&self) -> f32 {
+        self.scale_y
+    }
+
+    pub fn scale_z(&self) -> f32 {
+        self.scale_z
+    }
+
+    pub fn inner(&self) -> &T {
+        self.inner
     }
 }
 
 pub struct ObjectBuilder<T> {
+    name: String,
+
     position_x: f32,
     position_y: f32,
     position_z: f32,
@@ -37,8 +123,10 @@ pub struct ObjectBuilder<T> {
 }
 
 impl<T> ObjectBuilder<T> {
-    pub fn new(inner: &T) -> Self {
+    pub fn new<S: Into<String>>(name: S, inner: &T) -> Self {
         Self {
+            name: name.into(),
+
             position_x: 0.0,
             position_y: 0.0,
             position_z: 0.0,
@@ -111,6 +199,8 @@ impl<T> ObjectBuilder<T> {
 
     pub fn build(&self) -> Object<T> {
         Object<T> {
+            name: self.name,
+
             position_x: self.position_x,
             position_y: self.position_y,
             position_z: self.position_z,
