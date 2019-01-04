4dce6030af408987a41642efb4deb5043086ae1d
Robert Węcławski <r.weclawski@gmail.com>
2019-01-04 16:42:44 +0100

    add normals

--- src/resource/mesh.rs
+++ src/resource/mesh.rs
@@ -11,10 +11,11 @@ use vulkano::device::Queue;
 #[derive(Clone,Copy)]
 pub struct Vertex {
     pub position: [f32; 3],
+    pub normal: [f32; 3],
     pub tex_coord: [f32; 2],
 }
 
-impl_vertex!(Vertex, position, tex_coord);
+impl_vertex!(Vertex, position, normal, tex_coord);
 
 /// Mesh is a collection of vertices, edges and faces that defines shape of object.
 pub struct Mesh {
